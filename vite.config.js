40ffcfebc2f65412d0b8e331a018c0201f5599de
rvargas-dev <renzo2303_@hotmail.com>
2025-02-05 01:12:07 -0500

    la

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -20,7 +19,10 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['vue'], // Asegura que Vue no se excluya del bundle
+      external: [], // Asegúrate de que Vue no sea externo
+    },
+    commonjsOptions: {
+      include: [/node_modules/], // Asegura que Vue esté incluido en el bundle
     },
   },
 });
