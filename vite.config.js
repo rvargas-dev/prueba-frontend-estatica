d6558891dc9b4905c3da4ea3a8f20bd0e91cc273
rvargas-dev <renzo2303_@hotmail.com>
2025-02-05 00:19:55 -0500

    cambios finakles 2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/prueba-frontend-estatica/",
+  base: "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)) 
