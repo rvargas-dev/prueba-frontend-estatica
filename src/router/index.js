50f8541cd59c73cf65dad416cd308a029968e70f
rvargas-dev <renzo2303_@hotmail.com>
2025-02-04 23:48:03 -0500

    cambios

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import login from '../components/login.vue';
 import reportList from '../components/reportList.vue';
 
@@ -9,7 +9,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/prueba-frontend-estatica/', 
+  base: "/prueba-frontend-estatica/", 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
